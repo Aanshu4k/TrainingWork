c42bac5435a8c840f342f715513bfae9e40687b1
Aanshu4k <aanshukumar3004@gmail.com>
2023-12-26 23:02:21 +0530

    Add form COmponent added

---
+++ myapp/src/Component_website/AddForm.jsx
@@ -0,0 +1,131 @@
+import Button from "react-bootstrap/Button";
+import { Col } from "react-bootstrap";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import axios from "axios";
+import React from "react";
+import { toast } from "react-hot-toast";
+import "./Products.css";
+
+const AddForm = (props) => {
+  const [newProduct, setNewProduct] = useState({
+      id: "",
+      prodName: "",
+      price: "",
+      brand: "",
+    });
+  const [validated, setValidated] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewProduct({ ...newProduct, [name]: value });
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+    setValidated(true);
+  };
+  const handleAdd = async (event) => {
+    event.preventDefault();
+    props.setShowAddForm(!props.showAddForm);
+    await axios
+      .post("https://localhost:7247/api/Product", newProduct)
+      .then((response) => {
+        console.log(response.data);
+        toast.success("Product Added Successfully!");
+      })
+      .catch((error) => toast.error("Error fetching data : ", error));
+    window.location.reload();
+  };
+  return (
+    <div style={{ marginTop: "20px", display: "block", width: "40rem" }}>
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleAdd}
+        className="add-form"
+      >
+        <h3>Add Product</h3>
+        <br />
+        <Form.Group as={Col} md="12" controlId="validationCustom01">
+          <FloatingLabel controlId="floatingInput" label="ID" className="mb-3">
+            <Form.Control
+              type="text"
+              name="id"
+              value={newProduct.id}
+              onChange={handleInputChange}
+              required
+              pattern="^[0-9]+$"
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter id in a valid format.
+            </Form.Control.Feedback>
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group as={Col} md="12" controlId="validationCustom02">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Product Name"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              name="prodName"
+              value={newProduct.prodName}
+              onChange={handleInputChange}
+              pattern="^[a-zA-Z0-9\s]+$"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Invalid Format
+            </Form.Control.Feedback>
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group as={Col} md="12" controlId="validationCustom03">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Price"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              name="price"
+              value={newProduct.price}
+              onChange={handleInputChange}
+              pattern="^\d*\.?\d*$"
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              Invalid Format
+            </Form.Control.Feedback>
+          </FloatingLabel>
+        </Form.Group>
+
+        <Form.Group as={Col} md="12" controlId="validationCustom04">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Brand"
+            className="mb-3"
+          >
+            <Form.Control
+              type="text"
+              name="brand"
+              value={newProduct.brand}
+              onChange={handleInputChange}
+              required
+            />
+          </FloatingLabel>
+        </Form.Group>
+
+        <Button type="submit" className="add-btn" onClick={handleAdd}>
+          ADD
+        </Button>
+      </Form>
+    </div>
+  );
+};
+export default AddForm;
--- myapp/src/Component_website/Products.jsx
+++ myapp/src/Component_website/Products.jsx
@@ -1,6 +1,5 @@
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
-import { Col } from "react-bootstrap";
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
 import { useState, useEffect } from "react";
@@ -8,14 +7,11 @@ import axios from "axios";
 import React from "react";
 import { toast, Toaster } from 'react-hot-toast';
 import './Products.css';
+import AddForm from "./AddForm";
 
 const CustDataTable = () => {
   const [prodData, setProdData] = useState([]);
   const [selectedProductId, setSelectedProductId] = useState(null);
-  const [newProduct, setNewProduct] = useState([{
-    id: '', prodName: '', price: '', brand: ''
-  }]);
-  const [validated, setValidated] = useState(false);
   const [showAddForm, setShowAddForm] = useState(false);
   const [productName, setProductName] = useState("");
   const [price, setPrice] = useState("");
@@ -30,30 +26,9 @@ const CustDataTable = () => {
       })
       .catch((error) => console.log("Error fetching data : ", error));
   }, []);
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewProduct({ ...newProduct, [name]: value })
-    const form = e.currentTarget;
-    if (form.checkValidity() === false) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-    setValidated(true);
-  }
 
-  const handleAdd = async (event) => {
-    event.preventDefault();
-    setShowAddForm(!showAddForm);
-    await axios
-      .post("https://localhost:7247/api/Product", newProduct)
-      .then((response) => {
-        console.log(response.data)
-        toast.success('Product Added Successfully!')
-      })
-      .catch((error) => toast.error("Error fetching data : ", error));
-    window.location.reload();
 
-  }
+  
   const handleEdit = (id, prodName, price, brand) => {
     setSelectedProductId(id);
     setProductName(prodName);
@@ -148,52 +123,9 @@ const CustDataTable = () => {
         </tbody>
       </Table>
       <Button type='submit' variant="primary" onClick={() => { setShowAddForm(!showAddForm) }} style={{ marginLeft: '45rem' }} >+ ADD</Button>
+
       {showAddForm && (
-        <div style={{ marginTop: "20px", display: 'block', width: '40rem' }}>
-          <Form noValidate validated={validated} onSubmit={handleAdd} className="add-form">
-            <h3>Add Product</h3><br />
-            <Form.Group as={Col} md="12" controlId="validationCustom01">
-              <FloatingLabel controlId="floatingInput" label="ID" className="mb-3">
-                <Form.Control type="text" name="id" value={newProduct.id} onChange={handleInputChange} required pattern="^[0-9]+$" />
-                <Form.Control.Feedback type="invalid">Please enter id in a valid format.</Form.Control.Feedback>
-              </FloatingLabel>
-            </Form.Group>
-
-            <Form.Group as={Col} md="12" controlId="validationCustom02">
-              <FloatingLabel controlId="floatingInput" label="Product Name" className="mb-3">
-                <Form.Control type="text" name="prodName" value={newProduct.prodName} onChange={handleInputChange} pattern="^[a-zA-Z0-9\s]+$" required />
-                <Form.Control.Feedback type="invalid">Invalid Format</Form.Control.Feedback>
-              </FloatingLabel>
-            </Form.Group>
-
-            <Form.Group as={Col} md="12" controlId="validationCustom03">
-              <FloatingLabel controlId="floatingInput" label="Price" className="mb-3">
-                <Form.Control
-                  type="text"
-                  name="price"
-                  value={newProduct.price}
-                  onChange={handleInputChange}
-                  pattern="^\d*\.?\d*$"
-                  required
-                />
-                <Form.Control.Feedback type="invalid">Invalid Format</Form.Control.Feedback>
-              </FloatingLabel>
-            </Form.Group>
-
-            <Form.Group as={Col} md="12" controlId="validationCustom03">
-              <FloatingLabel controlId="floatingInput" label="Brand" className="mb-3">
-                <Form.Control type="text"
-                  name="brand" value={newProduct.brand}
-                  onChange={handleInputChange} required />
-              </FloatingLabel>
-            </Form.Group>
-
-            <Button type="submit" className="add-btn"
-              onClick={handleAdd}>
-              ADD
-            </Button>
-          </Form>
-        </div>
+        <AddForm setShowAddForm={setShowAddForm} showAddForm={showAddForm}/>
       )}
 
       {showForm && (
